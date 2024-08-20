ebef00d4f51d2819540aef38f4c5eeb0a30530ef
gabrielpaixaoz <gabrielpaixao54@gmail.com>
2024-08-20 16:32:08 -0300

    jkn

--- algoritm/prototype.js
+++ algoritm/prototype.js
@@ -3,4 +3,4 @@ const game2 = [2.47,3.05,3.40]
 let multiplicateFactor = game2[0]
 let part1 =  game1.map(item => +(item * multiplicateFactor - 1).toFixed(2))
 parseFloat(part1)
-console.log(part1)
+console.log(part1)  
