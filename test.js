0e268934fe7081f1902aeaa26e506e3a60894d63
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-08-28 16:07:20 -0300

    map investmento

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const VALOR_INVESTIDO = 100
 let odds = [3.00,3.80,3.50]
 let probabilidade_geral = odds.map(valor => 1 / valor).reduce((a, b) => a+ b, 0)
-let investimentos = odds.map(valor => (VALOR_INVESTIDO/valor)/probabilidade_geral)
+let investimentos = odds.map(valor => (VALOR_INVESTIDO/valor)/probabilidade_geral).map(valor => valor.toFixed(2))
 let retornos = investimentos.map((item, index) => item * odds[index])
 let lucro = retornos[0] - VALOR_INVESTIDO
 const CONTAS = 4 
